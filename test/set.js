c7d7989b28a6d5f0fffa4a2badac652ed682519b
calidion <calidion@gmail.com>
2016-07-05 16:23:19 +0800

    updated

--- test/set.js
+++ test/set.js
@@ -1,7 +1,7 @@
 'use strict';
 var bodyParser = require('body-parser');
 var assert = require('assert');
-var weixinConfigRouter = require('../lib/router');
+var setter = require('../lib/set');
 var express = require('express')();
 var settings = require('node-weixin-settings');
 var fs = require('fs');
@@ -15,34 +15,54 @@ express.use(bodyParser.raw({
   type: 'text/xml'
 }));
 
-var router = weixinConfigRouter('id', settings, function callback(req, res, id, value) {
-  res.json({
-    id: id,
-    data: value
-  });
-});
+// var router = weixinConfigRouter.set('id', settings, function callback(req, res, id, value) {
+//   res.json({
+//     id: id,
+//     data: value
+//   });
+// });
 
-express.use('/weixin/:id', router);
+console.log(setter);
+
+setter.set(
+  settings,
+  express,
+  // 数据处理完成后的回调函数
+  // ID是保存数据唯一识别号，可以自己定义
+  // value是所获取或者保存的数据
+  function callback(req, res, id, value) {
+    res.json({
+      id: String(id),
+      data: value
+    });
+  },
+  function () {
+    return 1;
+  },
+  // 获取数据识别ID的参数名
+  'appId',
+  '/weixin/config/:type'
+);
 
 
-describe('request', function() {
-  it('should have attributes', function() {
-    assert.equal(true, router.__handlers instanceof Object);
-    assert.equal(true, router.__handlers.get instanceof Function);
-    assert.equal(true, router.__handlers.post instanceof Function);
-  });
+describe('request', function () {
+  // it('should have attributes', function () {
+  //   assert.equal(true, router.__handlers instanceof Object);
+  //   assert.equal(true, router.__handlers.get instanceof Function);
+  //   assert.equal(true, router.__handlers.post instanceof Function);
+  // });
 
-  it('should set app config', function(done) {
+  it('should set app config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/app')
+      .post('/weixin/config/app')
       .send({
         id: '1',
         token: 'token',
         secret: 'secret'
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         console.log(res.body);
         assert.equal(true, res.body.id === '1');
@@ -53,12 +73,12 @@ describe('request', function() {
       });
   });
 
-  it('should get app config', function(done) {
+  it('should get app config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/app')
+      .get('/weixin/config/app')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data !== null);
@@ -69,16 +89,16 @@ describe('request', function() {
       });
   });
 
-  it('should set oauth config', function(done) {
+  it('should set oauth config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/oauth')
+      .post('/weixin/config/oauth')
       .send({
         state: 'state',
         scope: 0
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.state === 'state');
@@ -87,12 +107,12 @@ describe('request', function() {
       });
   });
 
-  it('should get oauth config', function(done) {
+  it('should get oauth config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/oauth')
+      .get('/weixin/config/oauth')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data !== null);
@@ -103,16 +123,16 @@ describe('request', function() {
   });
 
 
-  it('should set merchant config', function(done) {
+  it('should set merchant config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/merchant')
+      .post('/weixin/config/merchant')
       .send({
         id: 'id',
         key: 'key'
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.id === 'id');
@@ -120,12 +140,12 @@ describe('request', function() {
         done();
       });
   });
-  it('should get merchant config', function(done) {
+  it('should get merchant config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/merchant')
+      .get('/weixin/config/merchant')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.id === 'id');
@@ -134,15 +154,15 @@ describe('request', function() {
       });
   });
 
-  it('should set message config', function(done) {
+  it('should set message config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/message')
+      .post('/weixin/config/message')
       .send({
         aes: 'aes'
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.aes === 'aes');
@@ -150,12 +170,12 @@ describe('request', function() {
       });
   });
 
-  it('should get message config', function(done) {
+  it('should get message config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/message')
+      .get('/weixin/config/message')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.aes === 'aes');
@@ -163,16 +183,16 @@ describe('request', function() {
       });
   });
 
-  it('should set server config', function(done) {
+  it('should set server config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/server')
+      .post('/weixin/config/server')
       .send({
         host: 'localhost',
         prefix: 'weixin'
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.host === 'localhost');
@@ -181,12 +201,12 @@ describe('request', function() {
       });
   });
 
-  it('should get server config', function(done) {
+  it('should get server config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/server')
+      .get('/weixin/config/server')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data.host === 'localhost');
@@ -195,15 +215,15 @@ describe('request', function() {
       });
   });
 
-  it('should set urls config', function(done) {
+  it('should set urls config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/urls')
+      .post('/weixin/config/urls')
       .send({
         url: 'http://localhost/weixin'
       })
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data !== null);
@@ -217,12 +237,12 @@ describe('request', function() {
       });
   });
 
-  it('should get urls config', function(done) {
+  it('should get urls config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/urls')
+      .get('/weixin/config/urls')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
         assert.equal(true, res.body.data !== null);
@@ -236,13 +256,13 @@ describe('request', function() {
       });
   });
 
-  it('should set certificate without pfx', function(done) {
+  it('should set certificate without pfx', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/certificate')
+      .post('/weixin/config/certificate')
       .field('pfxKey', 'key')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         // var content = fs.readFileSync(__dirname + '/fixtures/cert.p12');
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
@@ -253,14 +273,14 @@ describe('request', function() {
       });
   });
 
-  it('should set certificate config', function(done) {
+  it('should set certificate config', function (done) {
     var request = require('supertest');
     request(express)
-      .post('/weixin/1/certificate')
+      .post('/weixin/config/certificate')
       .field('pfxKey', 'key')
       .attach('pfx', __dirname + '/fixtures/cert.p12')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         var content = fs.readFileSync(__dirname + '/fixtures/cert.p12');
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
@@ -271,12 +291,12 @@ describe('request', function() {
       });
   });
 
-  it('should get certificate config', function(done) {
+  it('should get certificate config', function (done) {
     var request = require('supertest');
     request(express)
-      .get('/weixin/1/certificate')
+      .get('/weixin/config/certificate')
       .expect(200)
-      .end(function(error, res) {
+      .end(function (error, res) {
         var content = fs.readFileSync(__dirname + '/fixtures/cert.p12');
         assert.equal(true, !error);
         assert.equal(true, res.body.id === '1');
@@ -285,9 +305,5 @@ describe('request', function() {
         assert.equal(true, res.body.data.pfxKey === 'key');
         done();
       });
-  });
-
-  it('should be set', function() {
-
   });
 });
